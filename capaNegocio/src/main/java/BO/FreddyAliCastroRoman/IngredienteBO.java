11c54bfc3a3e741a2bbd520eb579060afb242853
FreddyACastroR <freddy.castro252191@potros.itson.edu.mx>
2025-04-04 17:31:41 -0700

    Avance Modulo Ingrediente (Capa Negocio) Plebes creo que es el patito.

--- capaNegocio/src/main/java/BO/FreddyAliCastroRoman/IngredienteBO.java
+++ capaNegocio/src/main/java/BO/FreddyAliCastroRoman/IngredienteBO.java
@@ -4,10 +4,77 @@
  */
 package BO.FreddyAliCastroRoman;
 
+import DAO.FreddyAliCastroRoman.IngredienteDAO;
+import DTO.FreddyAliCastroRoman.IngredienteNuevoDTO;
+import DTO.FreddyAliCastroRoman.IngredienteViejoDTO;
+import Exception.ModuloIngredienteNegocioException;
+import Exception.ModuloIngredientePersistenciaException;
+import Mapper.IngredienteMapper;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author LyFredD
  */
 public class IngredienteBO {
+    
+    private static IngredienteBO instance;
+    private static IngredienteDAO ingredienteDAO;
+    
+    private IngredienteBO() {
+        this.ingredienteDAO = IngredienteDAO.getInstance();
+    }
+    
+    public static IngredienteBO getInstance() {
+        if (instance == null) {
+            instance = new IngredienteBO();
+        }
+        return instance;
+    }
+    
+    
+    public IngredienteViejoDTO agregarIngrediente(IngredienteNuevoDTO ingrediente) throws ModuloIngredienteNegocioException {
+        
+        if (ingrediente == null) throw new ModuloIngredienteNegocioException("El ingrediente no puede ser nulo.");
+        
+        if (ingrediente.getNombre() == null || ingrediente.getNombre().trim().isEmpty()) 
+            throw new ModuloIngredienteNegocioException("El nombre del ingrediente no puede ser nulo o vacio.");
+        
+        if (ingrediente.getUnidadMedida() == null) 
+            throw new ModuloIngredienteNegocioException("La unidad de medida del ingrediente no puede ser nula.");
+        
+        if (ingrediente.getStock() == null || ingrediente.getStock() <= 0) 
+            throw new ModuloIngredienteNegocioException("El stock del ingrediente debe ser mayor a cero.");
+        
+        try {
+            return IngredienteMapper.toViejoDTO(ingredienteDAO.agregarIngrediente(IngredienteMapper.toEntity(ingrediente)));
+        } catch (ModuloIngredientePersistenciaException ex) {
+            Logger.getLogger(IngredienteBO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ModuloIngredienteNegocioException("Ocurrió un error al agregar el ingrediente: " + ex.getMessage());
+        }
+    }
+    
+    
+    public IngredienteViejoDTO editarIngrediente(IngredienteViejoDTO ingrediente) throws ModuloIngredienteNegocioException {
+         
+        if (ingrediente == null) throw new ModuloIngredienteNegocioException("El ingrediente no puede ser nulo.");
+        
+        if (ingrediente.getNombre() == null || ingrediente.getNombre().trim().isEmpty()) 
+            throw new ModuloIngredienteNegocioException("El nombre del ingrediente no puede ser nulo o vacio.");
+        
+        if (ingrediente.getUnidadMedida() == null) 
+            throw new ModuloIngredienteNegocioException("La unidad de medida del ingrediente no puede ser nula.");
+        
+        if (ingrediente.getStock() == null || ingrediente.getStock() <= 0) 
+            throw new ModuloIngredienteNegocioException("El stock del ingrediente debe ser mayor a cero.");
+        
+        try {
+            return IngredienteMapper.toViejoDTO(ingredienteDAO.editarIngrediente(ingrediente.getId(), IngredienteMapper.toEntity(ingrediente)));
+        } catch (ModuloIngredientePersistenciaException ex) {
+            Logger.getLogger(IngredienteBO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ModuloIngredienteNegocioException("Ocurrió un error al editar el ingrediente: " + ex.getMessage());
+        }
+    }
     
 }
